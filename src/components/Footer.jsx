bc51f50604de5a9a97fbeffa1c28837a076af1b9
Manuel Frezzato <manuel.frezzato@gmail.com>
2023-12-05 01:11:37 +0100

    Feat| Esercizio senza repo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,51 @@
-import { useState } from "react"
-const Footer = () =>{
-    const [count, setCount] = useState()
-    return(
+import { useState } from "react";
+import "./Footer.css";
+const Footer = () => {
+  const [count, setCount] = useState({
+    name: "Tizio",
+    age: 40,
+    occupation: "student",
+  });
+  const [newValue, setNewValue] = useState("");
+  const [selectedKey, setselectedKey] = useState("");
 
-    )
-}
-export default Footer
+  return (
+    <article className="log-in">
+      <div>
+        <h1>REGISTRATI QUI</h1>
+        <ul>
+          <li>Name: {count.name}</li>
+          <li>Age: {count.age}</li>
+          <li>Occupation: {count.occupation}</li>
+        </ul>
+      </div>
+      <div className="registrazione">
+        <h3>sostituisci i dati</h3>
+        <select
+          value={selectedKey}
+          onChange={(e) => setselectedKey(e.target.value)}
+        >
+          <option value="name">Name</option>
+          <option value="age">Age</option>
+          <option value="occupation">Occupation</option>
+        </select>
+        <input
+          type="text"
+          value={newValue}
+          onChange={(e) => setNewValue(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            setCount((c) => ({
+              ...c,
+              [selectedKey]: newValue,
+            }));
+          }}
+        >
+          sostituisci
+        </button>
+      </div>
+    </article>
+  );
+};
+export default Footer;
